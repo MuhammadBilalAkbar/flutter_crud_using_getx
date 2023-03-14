49cdf48f15160523098892d22af3819a127001da
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-03-14 22:01:54 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,5 @@
-import 'package:flutter_crud_using_getx/pages/create_page.dart';
-import 'package:flutter_crud_using_getx/pages/edit_page.dart';
-import 'package:flutter_crud_using_getx/pages/main_page.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'my_app.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Get X CRUD api',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      initialRoute: '/mainPage',
-      getPages: [
-        GetPage(name: '/mainPage', page: () => MainPage()),
-        GetPage(name: '/addPage', page: () => CreatePage()),
-        GetPage(name: '/editPage', page: () => EditPage()),
-      ],
-    );
-  }
-}
+void main() => runApp(const MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_crud_using_getx/main.dart';
+// import 'package:flutter_crud_using_getx/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:flutter_crud_using_getx/my_app.dart';
+import 'package:flutter_crud_using_getx/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
